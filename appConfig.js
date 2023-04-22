bcc2e08b40ed00917880ec8337ad76af8367ea09
jeyus84 <120327102+jeyus84@users.noreply.github.com>
2023-04-22 23:35:17 +0700

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Before And After",
+  tagline: "All About Before And After",
+  description: "Collection Of Before And After Image Eround The Net .",
+  keywords: "before and after photo,before and after surgery,botox before and after,weight loss before and after,micro needling before and after",
+  author: "cDOM",
+  favicon: "https://pugjs.org/images/favicon.ico",
+  imageCount: 10,
+  removeBadWords: "porn,nude,sex",
+};
